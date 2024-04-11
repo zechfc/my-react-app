0f1863198f82b8b96f99e7a07e5575fef822db07
zechfc <zcf@email.edu.sc>
2024-04-11 09:28:57 -0400

    React

---
+++ src/Animal.js
@@ -0,0 +1,13 @@
+import './Animal.css';
+
+const Animal = () => {
+  return (
+  <section className = {Animal.className}>
+    <h3>(animal.name)</h3>
+    <p>Pandas are great</p>
+
+  </section>
+  );
+}
+
+export default Animal;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Animal from './Animal';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Header />
+
+    <Animal name="Panda" className="Panda"/>
+    
+    <Animal name="Tiger" className="Tiger"/>
+
+    <Footer />
     </div>
+    
+
   );
 }
 
